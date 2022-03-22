7df395cc3615fe287443f52043090928ab423890
MichaCham <cham7694-github@yahoo.com>
2022-03-22 10:34:09 +0100

    Avancement Fourrier

--- Fourrier/p_vecteur.c
+++ Fourrier/p_vecteur.c
@@ -86,6 +86,21 @@ void doubler_vecteur_fourrier(int nb_vecteur){
 
 }
 
+void dedoubler(t_vecteur* ceci){
+    for (int i = 0; i < mon_nb_vecteurs_de_fourier; i++)
+    {
+        ceci->->les_valeurs[i+mon_nb_vecteurs_de_fourier] = ceci->les_valeurs[i];
+    }
+    
+}
+
+void dedoubler_oppose(t_vecteur* ceci){
+    for (int i = 0; i < mon_nb_vecteurs_de_fourier; i++)
+    {
+        ceci->->les_valeurs[i+mon_nb_vecteurs_de_fourier] = -ceci->les_valeurs[i];
+    }
+}
+
 
 /**
  * Initalise la bibliothèque pour réaliser les calculs de transformation de fourier rapide.
@@ -96,8 +111,54 @@ void initialiser_base_fourier(){
     for (int i = 0; i < mon_nb_vecteurs_de_fourier; i++)
     {
         if(mon_nb_vecteurs_de_fourier>=TAILLE_SIGNAL){
-            return;
+            copie_vecteur(&(mes_vecteurs_de_fourier[i]),&(mes_vecteurs_de_fourier[i+mon_nb_vecteurs_de_fourier]));
+            dedoubler(&(mes_vecteurs_de_fourier[i]));
+            dedoubler_oppose(&(mes_vecteurs_de_fourier[i+mon_nb_vecteurs_de_fourier]));
         }
+    }
+    
+}
+
+void vecteur_base_fourier(int i, t_vecteur* f_i){
+    copie_vecteur(&(mes_vecteurs_de_fourier[i]), f_i);
+}
+
+/**
+ * Copie la transformée de <*ceci> dans <*transforme>.
+ * @param ceci : const t_vecteur*.
+ * @param transforme : t_vecteur*.
+ */
+void transformer_vecteur(const t_vecteur* ceci, t_vecteur* transforme){
+    for (int i = 0; i < TAILLE_SIGNAL; i++)
+    {
+        transforme->les_valeurs[i] = produit_scalaire(&(mes_vecteurs_de_fourier[i]), ceci) / (float)TAILLE_SIGNAL;
+    }
+    
+}
+
+
+void ajoute_multiple_vecteur(const t_vecteur* ceci, float coeff, t_vecteur* accumulateur){
+    for (int i = 0; i < TAILLE_SIGNAL; i++)
+    {
+        accumulateur->les_valeurs[i] += coeff * ceci->les_valeurs[i];
+    }
+    
+}
+
+/**
+ * Copie le vecteur original de la transformée <*ceci> dans <*original>.
+ * @param ceci : const t_vecteur*.
+ * @param original : t_vecteur*.
+ */
+void reformer_vecteur(const t_vecteur* ceci, t_vecteur* original){
+    for (int i = 0; i < TAILLE_SIGNAL; i++)
+    {
+        original->les_valeurs[i]= 0.0f;
+    }
+    for (int i = 0; i < TAILLE_SIGNAL; i++)
+    {
+        ajoute_multiple_vecteur(&mes_vecteurs_de_fourier[i], original->les_valeurs[i], original);
+
     }
     
 }
